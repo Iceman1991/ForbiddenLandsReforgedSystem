9de1c54270a106739b34e269062f4ab296da4c73
Christian Wagner <cwagner372@gmail.com>
2024-09-14 12:32:57 +0200

    changed weight parameters

--- forbidden-lands.js
+++ forbidden-lands.js
@@ -2325,7 +2325,20 @@
                 5: 5,
                 6: 6,
                 7: 7,
-                8: 8
+                8: 8,
+                9: 9,
+                10: 10,
+                11: 11,
+                12: 12,
+                13: 13,
+                14: 14,
+                15: 15,
+                16: 16,
+                17: 17,
+                18: 18,
+                19: 19,
+                20: 20
+
             },
             enrichedActorFields: ["note", "pride", "face", "body", "clothing", "darkSecret"],
             enrichedItemFields: ["description", "effect", "drawback", "appearance", "tools", "features.others"],
